e35ed90ebed66b042e11029b320532a3f9b81f9f
ArkDev <me@braydonb.com>
2025-05-16 10:16:22 -0700

    Added prepublish script to write version.

---
+++ bin/writeVersion.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// Ran from root as prepublishOnly hook to replace __SDK_DEV_VERSION__ with the actual SDK version.
+const fs = require('fs');
+const package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const server = fs.readFileSync('./server.js', 'utf8');
+
+fs.writeFileSync('./server.js', server.replace(/__DEV_SDK_VERSION__/g, package.version));
