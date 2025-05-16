5d754ca54cc0aa090e97fd6b045ffef63bbf7059
ArkDev <me@braydonb.com>
2025-05-16 11:12:55 -0700

    Bump package and added check to prevent version mismatches.

--- bin/writeVersion.js
+++ bin/writeVersion.js
@@ -5,4 +5,8 @@ const fs = require('fs');
 const package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const server = fs.readFileSync('./server.js', 'utf8');
 
+if (!server.includes('__DEV_SDK_VERSION__')) {
+  throw new Error('__DEV_SDK_VERSION__ not found in server.js. Please create a fresh build before publishing with: cd ../server && bun run build.');
+}
+
 fs.writeFileSync('./server.js', server.replace(/__DEV_SDK_VERSION__/g, package.version));
