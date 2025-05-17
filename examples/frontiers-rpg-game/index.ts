0fc06fab04c1e470a0152f18608649fb7ae17436
ArkDev <me@braydonb.com>
2025-05-16 17:46:12 -0700

    Commit initial assets

--- examples/frontiers-rpg-game/index.ts
+++ examples/frontiers-rpg-game/index.ts
@@ -2,7 +2,9 @@ import {
   startServer,
   Audio,
   DefaultPlayerEntity,
+  Entity,
   PlayerEvent,
+  ColliderShape,
 } from 'hytopia';
 
 import worldMap from './assets/maps/farm-start.json';
@@ -19,6 +21,42 @@ import worldMap from './assets/maps/farm-start.json';
 
 startServer(world => {
   world.loadMap(worldMap);
+
+  // const rodent1 = new Entity({
+  //   name: 'Rodent',
+  //   modelUri: 'models/npcs/adventurer.gltf',
+  //   modelPreferredShape: ColliderShape.BLOCK,
+  //   modelLoopedAnimations: [ 'idle' ],
+  //   modelScale: 0.75,
+  // });
+  // rodent1.spawn(world, { x: -2, y: 10, z: 0 });
+
+  // const rodent2 = new Entity({
+  //   name: 'Rodent',
+  //   modelUri: 'models/npcs/farmer.gltf',
+  //   modelPreferredShape: ColliderShape.BLOCK,
+  //   modelLoopedAnimations: [ 'idle' ],
+  //   modelScale: 0.75,
+  // });
+  // rodent2.spawn(world, { x: 0, y: 10, z: 0 });
+
+  // const rodent3 = new Entity({
+  //   name: 'Rodent',
+  //   modelUri: 'models/npcs/miner.gltf',
+  //   modelPreferredShape: ColliderShape.BLOCK,
+  //   modelLoopedAnimations: [ 'idle' ],
+  //   modelScale: 0.75,
+  // });
+  // rodent3.spawn(world, { x: 2, y: 10, z: 0 });
+
+  const rodent4 = new Entity({
+    name: 'Rodent',
+    modelUri: 'models/npcs/mushroom-boy.gltf',
+    modelPreferredShape: ColliderShape.BLOCK,
+    modelLoopedAnimations: [ 'idle' ],
+    modelScale: 0.5,
+  });
+  rodent4.spawn(world, { x: 0, y: 10, z: 0 });
 
   world.on(PlayerEvent.JOINED_WORLD, ({ player }) => {
     const playerEntity = new DefaultPlayerEntity({
